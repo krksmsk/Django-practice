661d0f9b21816a20fa87e414022865c091f8acc8
krksmsk <59610126+krksmsk@users.noreply.github.com>
2020-06-06 16:36:03 +0900

    create_model_by_admin

--- hello_world/admin.py
+++ hello_world/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Galpan_character
 # Register your models here.
+admin.site.register(Galpan_character)
--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Galpan_character(models.Model):
+    name = models.CharField(max_length=20)
+    text = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.name
