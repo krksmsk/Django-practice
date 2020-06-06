25e030c2f962a01b7bfc2c1c58af464c1a76bb56
krksmsk <59610126+krksmsk@users.noreply.github.com>
2020-06-06 13:52:17 +0900

    create_HelloWorld

--- sample_app/settings.py
+++ sample_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hello_world.apps.HelloWorldConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
